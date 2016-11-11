a978dd8842f67e9e943c8c7ecc65d6a2558e1b41
Veeti Karttunen <K1565@student.jamk.fi>
2016-11-11 15:16:28 +0200

    init

---
+++ ConsoleApplication1/ConsoleApplication1/Dijkstra.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Dijkstra
+    {
+        public int[,] Matriisi { get; set; }
+        private int[] unvisited, visited, distance, via;
+        private int start, current;
+        private bool finished;
+
+        public Dijkstra(int[,] matriisi, int s)
+        {
+            finished = false;
+            start = s;
+            current = start;
+            Matriisi = matriisi;
+            unvisited = new int[(int)Math.Sqrt(matriisi.Length)];
+            visited = new int[(int)Math.Sqrt(matriisi.Length)];
+            distance = new int[(int)Math.Sqrt(matriisi.Length)];
+            via = new int[(int)Math.Sqrt(matriisi.Length)];
+            for (int i = 0; i < distance.Length; i++)
+            {
+                distance[i] = int.MaxValue;
+                visited[i] = -1;
+                via[i] = -1;
+            }
+            for (int i = 0; i < Math.Sqrt(matriisi.Length); i++)
+            {
+                unvisited[i] = i;
+            }
+        }
+        public void Calculate()
+        {
+            int[] tdistance = new int[(int)Math.Sqrt(Matriisi.Length)];
+            while (finished != true)
+            {
+                if (current == start)
+                {
+                    distance[current] = 0;
+                    tdistance = distance;
+                    via[current] = -1;
+                }
+                for (int i = 0; i < distance.Length; i++)
+                {
+                    if (Matriisi[current, i] != 0 && Matriisi[current, i] + distance[current] < distance[i])
+                    {
+                        distance[i] = distance[current] + Matriisi[current, i];
+                        tdistance = distance;
+                        via[i] = current;
+                    }
+                }
+                Array.Sort(tdistance);
+                visited[current] = current;
+                unvisited[current] = -1;
+                for (int i = 0; i < tdistance.Length; i++)
+                {
+                    if (tdistance[i] > distance[current])
+                    {
+                        current = Array.FindIndex(distance, item => item == tdistance[i]);
+                        break;
+                    }
+                }
+                if (Array.FindIndex(visited, item => item == -1) == -1) { finished = true; }
+            }
+            Console.WriteLine("Embryo:  Distance:   Via:");
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.Write(" {0}         ", visited[i]);
+                Console.Write("{0}          ", distance[i]);
+                if (via[i] == -1) { Console.WriteLine(" "); }
+                else { Console.WriteLine(via[i]); }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Matriisi = new int[8, 8];
+            Matriisi[0, 1] = 10;
+            Matriisi[0, 2] = 15;
+            Matriisi[1, 0] = 10;
+            Matriisi[1, 3] = 17;
+            Matriisi[1, 4] = 16;
+            Matriisi[2, 0] = 15;
+            Matriisi[2, 4] = 24;
+            Matriisi[2, 5] = 45;
+            Matriisi[2, 6] = 15;
+            Matriisi[2, 7] = 45;
+            Matriisi[3, 1] = 17;
+            Matriisi[3, 4] = 12;
+            Matriisi[4, 1] = 16;
+            Matriisi[4, 2] = 24;
+            Matriisi[4, 3] = 12;
+            Matriisi[4, 7] = 19;
+            Matriisi[5, 2] = 45;
+            Matriisi[5, 6] = 11;
+            Matriisi[5, 7] = 19;
+            Matriisi[6, 2] = 15;
+            Matriisi[6, 5] = 11;
+            Matriisi[6, 7] = 21;
+            Matriisi[7, 2] = 45;
+            Matriisi[7, 4] = 19;
+            Matriisi[7, 5] = 19;
+            Matriisi[7, 6] = 21;
+            Dijkstra Algoritmi = new Dijkstra(Matriisi, 7);
+            Algoritmi.Calculate();
+        }
+    }
+}
